0cdeb34265da9a0e7266fc624bf1fe587c1a66e3
Ayo <Ayoroq@gmail.com>
2025-12-10 10:03:21 -0500

    Added the html files with basic information

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const http = require("http");
+const fs = require("fs");
 
-const server = http.createServer((req, res) => console.log("request made"));
+const server = http.createServer((req, res) => 
+    console.log(req.url, req.method)
+);
 
 server.listen(8080, "localhost", () => {
   console.log("listening for requests on port 8080");
